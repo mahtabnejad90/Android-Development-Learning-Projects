d260e777ff30926e546c7f7ea5e03deb9e62e73e
Unknown <mahtab.nejad@sky.uk>
2019-06-09 17:29:53 +0100

    added kotlin tutorial project
    -started with variables
    -declaration of variables
    -string and int variable types
    -coding conventions.
    -basic println functions.

---
+++ Learning-Resources/KotlinTutorial/src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+//fun defines function in kotlin
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("Mahtab has started Kotlin development")
+
+    //naming must be exact as variables declared are case sensitive
+    //convention; declare variables in lower capital (camel case)
+    //classes start in capital letters
+    //make sure varable names are specific so that they can reflect their purpose or value
+
+    //declaring string variable - different ways of declaring variables.
+
+    //String:
+    var mahtab: String = "Mahtab Nejad"
+    println(mahtab)
+
+    var nejad: String
+    nejad = "Bahman Nejad"
+    println(nejad)
+
+    var stringNumber : String = "32"
+
+    //Integers & numbers:
+
+    var mahtabsWeeklySalary: Int = 32
+    var mahtabsMonthlySalary: Int = mahtabsWeeklySalary * 4
+    println(mahtabsWeeklySalary)
+    println(mahtabsMonthlySalary)
+
+
+
+
+
+
+}
