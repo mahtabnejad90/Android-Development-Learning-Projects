6a358cb28f2a2ea43a3bfe457bd1c247b2571bcc
Unknown <mahtab.nejad@sky.uk>
2019-08-06 08:32:12 +0100

    kotlin for loop continued
    for each loop

--- KotlinTutorial/src/main/kotlin/Main.kt
+++ KotlinTutorial/src/main/kotlin/Main.kt
@@ -81,7 +81,7 @@ println(enemy)
     enemy.takeDamage(11)
     println(enemy)*/
 
-    val uglyTroll = Troll("Ugly Troll")
+   /* val uglyTroll = Troll("Ugly Troll")
     println(uglyTroll)
     uglyTroll.takeDamage(30)
     println(uglyTroll)
@@ -91,18 +91,44 @@ println(enemy)
     vlad.takeDamage(8)
     println(vlad)
 
-    val dracula = VampyreKing("Dracula")
-    println(dracula)
 
-    while (dracula.lives > 0) {
-        if (dracula.dodges()){
-            continue;
+    */
+
+    for (i in 1 .. 10) {
+        val dracula = VampyreKing("Dracula")
+        println(dracula)
+
+        while (dracula.lives > 0) {
+            if (dracula.dodges()) {
+                continue;
+            }
+            if (dracula.runAway()) {
+                println("Dracula ran away")
+                break
+            } else {
+                dracula.takeDamage(80)
+            }
         }
-        if (dracula.runAway()) {
-            println("Dracula ran away")
-            break
-        } else {
-            dracula.takeDamage(12)
-        }
+        println("--------------------------")
     }
+
+    //for each loop
+/*
+val conan = Player("Conan")
+    conan.getLoot(Loot("Invisibility", LootType.POTION, 4.0))
+    conan.getLoot(Loot("Mithril", LootType.ARMOR, 183.0))
+    conan.getLoot(Loot("Ring of Speed", LootType.RING, 25.0))
+    conan.getLoot(Loot("Red Potion", LootType.POTION, 2.0))
+    //conan.getLoot(Loot("Cursed Shield", LootType.ARMOR, -8.0))
+    conan.getLoot(Loot("Brass Ring", LootType.RING, 1.0))
+    conan.getLoot(Loot("Chain Mail", LootType.ARMOR, 4.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
+    conan.getLoot(Loot("Health Potion", LootType.POTION, 3.0))
+    conan.getLoot(Loot("Silver Ring", LootType.RING, 6.0))
+
+    conan.showInventory()
+*/
+
+
+
 }
