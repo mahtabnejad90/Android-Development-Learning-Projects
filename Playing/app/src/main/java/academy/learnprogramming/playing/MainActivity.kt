cbf3e92759d42f289a98bca70d6f1419a6b12bed
Unknown <mahtab.nejad@sky.uk>
2019-08-16 08:42:12 +0100

    Fix gradle issue by deleting &repasting project into folder

--- Playing/app/src/main/java/academy/learnprogramming/playing/MainActivity.kt
+++ Playing/app/src/main/java/academy/learnprogramming/playing/MainActivity.kt
@@ -1,7 +1,7 @@
-package learningprogramming.academy
+package academy.learnprogramming.playing
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
