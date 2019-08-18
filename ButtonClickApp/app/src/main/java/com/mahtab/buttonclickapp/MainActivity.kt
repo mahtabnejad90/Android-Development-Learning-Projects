053bcede0885a0ba3e29a7d553fc9b1ce21cc78a
Unknown <mahtab.nejad@sky.uk>
2019-08-18 16:40:42 +0100

    added widgets + onclick functionality
    added:
    -Widget libraries.
    -Button widget
    -Textview & Edit Text widgets
    -Added OnClick functionality
    -Added ScrollingMovement Method
    -Added clearing text functionality at start of app and after user uses the button.setOnClickListener

--- ButtonClickApp/app/src/main/java/com/mahtab/buttonclickapp/MainActivity.kt
+++ ButtonClickApp/app/src/main/java/com/mahtab/buttonclickapp/MainActivity.kt
@@ -2,11 +2,65 @@ package com.mahtab.buttonclickapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //private var userInput: EditText? = null
+    //private var button: Button? = null
+    private var textView: TextView? = null
+    //private var numTimesClicked = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+        userInput.setText("")
+
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+//numTimesClicked += 1
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                /*if (numTimesClicked != 1) {
+                    textView?.append("s\n")
+                } else {
+                    textView?.append("\n")
+                }*/
+                //userInput.text.clear()
+                userInput.setText("")
+            }
+
+        })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
